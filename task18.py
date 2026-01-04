8c8a2bd766e19e54271502d659e993562deaea48
Nodirbek <berdimurodovnodir89@gmail.com>
2026-01-04 15:31:06 +0500

    bajarildi

---
+++ task18.py
@@ -0,0 +1,8 @@
+students = {"name": "Ali", "age": 25, "grade": "A"}
+
+
+for kalit,value in students.items():
+    print(f'kalit : {kalit.upper()}')
+    print(f'value : {value}')
+
+print(students)
